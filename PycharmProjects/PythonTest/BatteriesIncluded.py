ebbf83bd157a5784e2b949ab7dd22fbc742e5bad
Zhaofei <zhaofei@zhaosha.com>
2018-10-25 13:37:28 +0800

    add skill concept ToAcquire file

--- PycharmProjects/PythonTest/BatteriesIncluded.py
+++ PycharmProjects/PythonTest/BatteriesIncluded.py
@@ -1,3 +1,94 @@
 
 # 常用内建模块
 
+#1.datetime 模块 ...
+
+from datetime import datetime
+
+print(datetime.now())
+
+#2. collections
+
+#2.1 namedtuple
+
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(1, 2)
+
+print('p.y =', p.y)
+
+print('is tuple', isinstance(p, tuple))
+
+#2.2 deque 提高插入和删除效率
+from collections import deque
+
+q = deque(['a', 'b', 'c'])
+q.append('x')
+q.appendleft('y')
+print('q =', q)
+q.popleft()
+print('q =', q)
+
+#2.3 defaultdict
+from collections import defaultdict
+dd = defaultdict(lambda: 'N/A')
+
+dd['key1'] = 'abc'
+print('key1:', dd['key1'], 'key2:', dd['key2'])
+
+#2.4 orderdict 实现FIFO队列
+from collections import OrderedDict
+
+d = dict([('a', 1), ('b', 2), ('c', 3)])
+
+print('d =', d)
+
+od = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+
+print('od =', d)
+
+class FIFOOrderDict(OrderedDict):
+    def __init__(self, capacity):
+        super(FIFOOrderDict, self).__init__()
+        self._capacity = capacity
+    def __setitem__(self, key, value):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
+
+fifoDict = FIFOOrderDict(2)
+fifoDict['a'] = 1
+fifoDict['b'] = 2
+print('fifo dict:', fifoDict)
+
+fifoDict['c'] = 3
+print('fifo dict:', fifoDict)
+
+fifoDict['d'] = 4
+print('fifo dict:', fifoDict)
+
+#chainMap 命令行 > 环境变量 > 默认参数 dict的 dict 内部会排序
+
+#user=admin color=green python3 use_chainmap.py -u bob
+
+#counter
+from collections import Counter
+
+c = Counter()
+for ch in 'Programming':
+    c[ch] = c[ch] + 1
+print('counter c:', c)
+
+print('type(c):', type(c))
+print('c is dict:', isinstance(c, dict))
+
+#3 base64
+
---
+++ TOAcquire.md
@@ -0,0 +1,38 @@
+规划- 终极目标
+
+概念学习
+最小化学习路径
+
+技术方向：
+
+iOS 增强 辅助 用户体验
+
+自动化测试/动画绘制
+
+PHP方向 后端开发 技术支持 强大的业务分析能力
+
+框架laravel Yii thinkphp 框架优劣
+数据库 memberCache/redis 缓存技术
+环境搭建与部署 高负载网站
+PHP架构 静态化设计 OOP编程思想
+NoSQL相关技术 索引/查询/存储 优化
+RPC？ SSH Linux命令行
+服务器 选择apache Nginx Tomcat部署
+scorm标准 Linux环境开发
+高并发 多线程
+数据结构和算法
+数据分析/数据挖掘
+设计模式
+单元测试
+小程序
+微服务
+音视频
+动画游戏
+
+相关语言：golang（go）python
+
+Java语言开发
+
+项目经理 架构师 CTO 
+
+数据方向 数据分析 更好的规划
