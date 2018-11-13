337be1adac5c199f398e2948fdd96ef8c8614a21
Zhaofei <zhaofei@zhaosha.com>
2018-11-13 13:46:03 +0800

    Add PHP Laravel framework study

--- PHP/flyTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$arrayName = array('1','3' );
-	$arrayName1 = array('1','4' );
-
-	echo var_dump(arrayName+arrayName1)."<br>";
-	echo var_dump(array_merge(arrayName,arrayName1));
-
-?>
--- PHP/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // define('APP_NAME','MyYard');
-    // define('APP_DEBUG',TRUE); // 开启调试模式
-    // define('APP_PATH','./MyYard/');
-
-    // require './ThinkPHP/ThinkPHP.php';
-
-	echo 321;
-?>
